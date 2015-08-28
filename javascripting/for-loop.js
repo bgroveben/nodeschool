227ebfdd1215d3dda784e4f7d136bd8398982d51
bgrove1974 <bgroveben@gmail.com>
2015-08-27 17:45:44 -0700

    Ifs and loops

---
+++ javascripting/for-loop.js
@@ -0,0 +1,13 @@
+total = 0;
+limit = 10;
+for(var i = 0; i < limit; i++) {
+	total += i
+}
+console.log(total)
+
+/*
+for (var i = 0; i < 10; i++) {
+  // log the numbers 0 through 9
+  console.log(i)
+}
+*/
