3f1ab46fe061ea3be481d5b590e2e2a1bf560f7f
bgrove1974 <bgroveben@gmail.com>
2015-08-31 01:32:13 -0700

    Begin Functional Javascript workshop

---
+++ functional_javascript/hello_world.js
@@ -0,0 +1,50 @@
+/*
+ FUNCTIONAL JAVASCRIPT IS GOOD
+───────────────────────────────
+ Hello World
+ Exercise 1 of 18
+
+
+# Task
+
+Write a function that takes an input string and returns it uppercased.
+
+## Arguments
+
+  * input: a String of random words (lorem ipsum).
+
+## Boilerplate
+
+    function upperCaser(input) {
+      // SOLUTION GOES HERE
+    }
+    
+    module.exports = upperCaser
+
+
+ » To print these instructions again, run: functional-javascript print
+ » To execute your program in a test environment, run: functional-javascript run program.js
+ » To verify your program, run: functional-javascript verify program.js
+ » For help run: functional-javascript help
+*/
+
+function upperCaser(input) {
+    return input.toUpperCase();
+}
+
+module.exports = upperCaser
+
+/*
+# PASS
+
+Your solution to Hello World passed!
+
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    module.exports = function(input) {
+      return input.toUpperCase()
+    }
+
+────────────────────────────────────────────────────────────────────────────────
+*/
